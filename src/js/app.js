9306b8c09e4b681e42298a1502607967a85fe428
Oleh Pshyk <pshyk.oleg@gmail.com>
2020-02-23 22:26:01 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
+import * as $ from 'jquery';
+import Popper from 'popper.js/dist/umd/popper';
+// import 'bootstrap';
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/popover';
+import 'bootstrap/js/dist/scrollspy';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/toast';
+import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/util';
+// window.Popper = Popper;
+if (module.hot) {
+  module.hot.accept();
+}
